b72bd848ce309f25d317dccabe30896678315e6c
smlrkh1107 <krae7789230@gmail.com>
2020-04-20 22:00:43 +0900

    탭 레이아웃 배치 / 페이지별 제목 설정

--- app/src/main/java/kun/hee/fragment_more/adapter/MyFragPagerAdapter.kt
+++ app/src/main/java/kun/hee/fragment_more/adapter/MyFragPagerAdapter.kt
@@ -11,13 +11,20 @@ class MyFragPagerAdapter(fm:FragmentManager) :FragmentPagerAdapter(fm){
     override fun getItem(position: Int): Fragment {
 //       getView 각각의 줄에 없던걸 뿌려줄꺼야. ListView의 getView와비슷
         return when (position) {
-            0 -> {MainFragment()}
-            1 -> {GameListFragment()}
-            else -> {BoardFragment()}
+            0 -> { MainFragment()}
+            1 -> { GameListFragment()}
+            else -> { BoardFragment() }
         }
-
-
     }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+
+            return when (position) {
+                0 -> "메인"
+                1 -> "게임리스트"
+                else -> "주식시장"
+            }
+        }
 
     override fun getCount(): Int {
 //        몇 페이지 지원할 거니 ? 우린 2개를 만들었단다.
