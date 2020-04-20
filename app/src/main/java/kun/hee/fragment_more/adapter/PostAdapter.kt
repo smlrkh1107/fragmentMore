d6f7e28e560606847c214dbd63039eacd91c7755
smlrkh1107 <krae7789230@gmail.com>
2020-04-20 20:45:31 +0900

    게시판 어댑터 기초작업

---
+++ app/src/main/java/kun/hee/fragment_more/adapter/PostAdapter.kt
@@ -0,0 +1,46 @@
+package kun.hee.listviewpractice.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import kun.hee.fragment_more.R
+import kun.hee.fragment_more.datas.Post
+import kun.hee.listviewpractice.datas.AppInformation
+
+class PostAdapter(context:Context, resId:Int, list:ArrayList<Post> ):ArrayAdapter<Post>(context, resId, list) {
+    val mContext = context
+    val mList = list
+    val inf = LayoutInflater.from(mContext)
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow =convertView
+//        tempRow?.Let{ NULL이 아닐떄,}.Let{NULL 일때}
+        if(tempRow == null) {
+            tempRow = inf.inflate(R.layout.post_item_list, null)
+        }
+//        tempRow?.Let{
+//        }.Let{ tempRow = inf.inflate(R.layout.play_store_list, null)}
+//
+
+        val row = tempRow!!
+//      ★ 요기 위에 하나도 모르겠음 ★
+
+        val
+
+        return row
+
+    }
+
+
+
+
+
+
+
+
+}
